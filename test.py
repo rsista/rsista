5dc3d45e841d099829a76ee2637c18409f8a117b
SRK <ravikumar.sista@gmail.com>
2021-10-24 21:54:21 +0530

    ravi commit

---
+++ test.py
@@ -0,0 +1,26 @@
+import numpy as np
+from math import *
+
+print("sista family")
+x= 10
+y=0.5
+z=3+2j
+i="ravi"
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(i))
+print(i.upper())
+print(i.lower())
+print(i.replace("r","J"))
+print("enter your name ")
+
+a =input()
+print("Hello,"+a)
+
+
+b= 3
+c = np.sin(b)
+print ( c )
+
+
